e7bc29ffb485879a2dd816d87496abed1cecb7ce
Shubhansh Programmer <shubhansh7777@gmail.com>
2021-05-24 09:00:47 +0530

    Programmer's Screen
    In this screen, i write about myself just showing who am i and the programming language also the  technologies used in this application

---
+++ src/Screens/AboutScreen.tsx
@@ -0,0 +1,23 @@
+import React  from 'react';
+
+import { View,
+         Text,
+         StyleSheet } from 'react-native';
+
+const AboutScreen = () => {
+  return (
+    <View>
+      <Text>Data</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+
+});
+
+export default AboutScreen;
+
+  
+
+
