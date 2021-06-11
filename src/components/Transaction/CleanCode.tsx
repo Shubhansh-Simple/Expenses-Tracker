bacc594e6a3bc279ea89652dbeb7b24a87b9a35f
Shubhansh Programmer <shubhansh7777@gmail.com>
2021-06-12 00:18:10 +0530

    Clean code file specially for transaction page.
    This is special like Django's file structure views.py same name in different django apps.
    This file contains mechanic methods which contains code make our main Transaction.tsx ugly.
    After adding that function this file our code become super clean.

---
+++ src/components/Transaction/CleanCode.tsx
@@ -0,0 +1,89 @@
+/*
+ * Method For Transaction Page
+ * which makes the main file
+ * clean and short.
+ */
+
+export function iconTitleSelector( data:string ){
+  /*
+   * All-Credit-Debit
+   * Icon Name
+   * Title
+   */
+  if( data==='0' ){
+    return {
+      'icon':'arrow-bottom-left',
+      'title' : 'Credit',
+    }
+  }
+  else if( data==='1' ){
+    return {
+      'icon':'arrow-top-right',
+      'title' : 'Debit',
+    }
+  }
+  else{
+    return {
+      'icon':'arrow-top-right-bottom-left',
+      'title' : 'All',
+    }
+  }
+}
+
+export function nextIteration( data:string ){
+  /*
+   * LIST iteration from 0-to-end-to-0
+   */
+  let x = Number(data)
+  { x===2 
+      ? 
+    x=x-2 
+      : 
+    x=x+1 }
+  return x.toString()
+}
+
+export function categoryQuerySelection( data:number ){
+  /*
+   *
+   */
+  if (data===0) {
+    return ''
+  }
+  else{
+    return ' AND SOURCE_NAME_ID='+data.toString()
+  }
+
+}
+
+
+export function NoDataTitle( data:string ){
+  /*
+   * Decide Title
+   * of NoDataFound 
+   * component using queryContainer 
+   * local storage
+   */
+  switch (data){
+    case '0' : {
+      return 'No Credit Transaction Found!'
+      break;
+    }
+    case '1' : {
+      return 'No Debit Transaction Found!'
+      break;
+    }
+    case '2' : {
+      return 'No Transaction Found!'
+      break;
+    }
+    default : {
+      console.log('Exception in component/Transaction/CleanCode/NoDataTitle')
+      break;
+    }
+  }
+
+}
+
+
+
